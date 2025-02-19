6d5633d9941502fb73d047fe32806e9e129930c3
Suprim Khatri <supremekhatri62@gmail.com>
2025-02-19 21:40:56 +0545

    deployment preparation

--- swiftCRM/settings.py
+++ swiftCRM/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-=-zqm1!&njmks^96$zqv^ry6lqun@ad#_z(svjqdmbepqh-mi4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['swiftcrm.onrender.com','localhost','127.0.0.1']
 
 
 # Application definition
